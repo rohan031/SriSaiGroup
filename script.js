7f5acdf3b69a5c770c3c290a2ba3ad576b85eb3c
rohan <rohanverma031@gmail.com>
2021-06-05 23:06:24 +0530

    scroll

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    let $active = $("#bbusiness, #aaboutUs, #ccommunity");
+    let $active = $("#bbusiness, #aaboutUs, #ccommunity, #ccontactUs");
     $active.click(function () {
         $active.addClass("active");
         $active.removeClass("active");
@@ -8,6 +8,7 @@ $(document).ready(function() {
         $(this).addClass("active");
     });
 
+    /*
     $("#community").mouseenter(function() {
         $active.addClass("active");
         $active.removeClass("active");
@@ -27,6 +28,55 @@ $(document).ready(function() {
         $active.removeClass("active");
 
         $("#bbusiness").addClass("active");
+    });
+
+    $("#contactUs").mouseenter(function() {
+       $active.addClass("active");
+       $active.removeClass("active");
+
+       $("#ccontactUs").addClass("active");
+    });
+    */
+
+
+    var screenHeight = $(window).height();
+    var funCall = screenHeight * 0.4;
+    var contactPos = $("#contactUs").offset().top - funCall;
+    var aboutPos = $("#aboutUS").offset().top - funCall;
+    var commPos = $("#community").offset().top - funCall;
+    var bussPos = $("#main").offset().top - funCall;
+
+    $(window).on("scroll", function(){
+       var y = window.pageYOffset;
+       if (y > contactPos) {
+           $active.addClass("active");
+           $active.removeClass("active");
+
+           $("#ccontactUs").addClass("active");
+           $("#footerContactUs").fadeIn("slow");
+       }
+       else if (y > commPos) {
+           $active.addClass("active");
+           $active.removeClass("active");
+
+           $("#ccommunity").addClass("active");
+           $("#footerContactUs").fadeOut("slow");
+       }
+       else if (y > aboutPos) {
+           $active.addClass("active");
+           $active.removeClass("active");
+
+           $("#aaboutUs").addClass("active");
+           $("#footerContactUs").fadeOut("slow");
+       }
+       else if (y > bussPos) {
+           $active.addClass("active");
+           $active.removeClass("active");
+
+           $("#bbusiness").addClass("active");
+           $("#footerContactUs").fadeOut("slow");
+       }
+
     });
 
 });
