64ee7bbf8704208976920da890aeee302e9e007f
rohan <rohanverma031@gmail.com>
2021-06-21 13:35:34 +0530

    mobile ui changes

--- js/script_mobile.js
+++ js/script_mobile.js
@@ -7,17 +7,20 @@ $(document).ready(function () {
         $side.css("width", "65%");
         $side.css("right", "0");
         $main.css("opacity", "0.3");
+        $main.addClass("not-active");
     });
 
     $("#sideClose").click(function () {
         $side.css("width", "0");
         $side.css("right", "-100%");
         $main.css("opacity", "1");
+        $main.removeClass("not-active");
     });
 
     $(".sidebarLinks").click(function () {
         $side.css("width", "0");
         $side.css("right", "-100%");
         $main.css("opacity", "1");
-    })
+        $main.removeClass("not-active");
+    });
 })
